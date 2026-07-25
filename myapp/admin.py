2cf8f258c466d69fc7a7bb9d9d2d0720b754f0d1
Diyorjon Raxmonqulov <coderdiyorjon0624@gmail.com>
2026-07-25 17:27:53 +0500

    feat
    customize model in django admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,5 +3,11 @@ from .models import Feature, Post
 
 # Register your models here.
 
-admin.site.register(Feature)
-admin.site.register(Post)
+@admin.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ('name', 'details')
+
+    
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body', 'created_at')
